f7e1b6c1720c5788663c587be31db8ee3c656227
Stephen Pontes <stephen@shibbay.com>
2017-04-14 12:27:32 -0700

    Updated to match comp more closely, active classes on nav, updated colors

--- dist/js/site.js
+++ dist/js/site.js
@@ -1,5 +1,18 @@
-var cards = document.querySelectorAll(".mc-color-card");
+var cards          = document.querySelectorAll(".mc-color-card"),
+    navLinks       = document.querySelectorAll('#sidebar a'),
+    navActiveClass = "active";
 
+function removeNavClasses() {
+    Array.prototype.forEach.call(navLinks, function(el, i){
+        // Remove all "active" classes
+        if (el.classList)
+          el.classList.remove(navActiveClass);
+        else
+          el.className = el.className.replace(new RegExp('(^|\\b)' + navActiveClass.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+    });
+}
+
+// Logic for getting/setting hex values on the color cards
 Array.prototype.forEach.call(cards, function(el, i){
     // Get the hex value from data attribute
     var hexValue  = el.getAttribute('data-color'),
@@ -11,4 +24,17 @@ Array.prototype.forEach.call(cards, function(el, i){
 
     // Inline style the hex value for the swatch
     hexSwatch.style.background = hexValue;
+});
+
+// Active classes / scrolling logic for navigation
+Array.prototype.forEach.call(navLinks, function(el, i){
+    el.addEventListener("click", function() {
+        removeNavClasses();
+
+        // Add active class
+        if (el.classList)
+          el.classList.add(navActiveClass);
+        else
+          el.className += ' ' + navActiveClass;
+    });
 });
